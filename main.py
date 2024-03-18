9fa840bcb94b2f74b161380c47054550d02109cd
Koromonnnnnnnn <764363@dpsk12.net>
2024-03-18 09:15:31 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+def calculate_distance(speed, time):
+    distance = speed * time
+    print(distance)
+    return distance
+
+
+def calculate_area_of_rectangle(length, width):
+    area = length * width
+    print(area)
+    return area
+
+
+calculate_distance(60, 3)
+calculate_area_of_rectangle(10, 5)
